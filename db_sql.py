3d8c0927d5a3a71759df78910cff55b829cbc30e
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-12-03 19:34:00 -0500

    Update db_sql.py

--- db_sql.py
+++ db_sql.py
@@ -8,9 +8,9 @@ from google.cloud import vision
 from google.cloud.vision import types, ImageAnnotatorClient
 import io
 import os
-import collections
-
-#################################################################
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
 
 def twitter_data(lines, count):
 	line_new = lines.split('"')
@@ -40,8 +40,6 @@ def twitter_data(lines, count):
 		handle = 'None'
 	return (user_name, handle, media_url, label)
 
-#################################################################
-
 def vision(img_name):
 	vision_client = ImageAnnotatorClient()
 	file_name = str(img_name)
@@ -53,16 +51,41 @@ def vision(img_name):
 	response = vision_client.label_detection(image=image)
 	labels = response.label_annotations
 	label = labels[0]
-
 	return label.description
-	# for label in labels:
-	# 	print(label.description, label.score)
 
-#################################################################
+def graph(obj, val, y_pos):
+	plt.bar(y_pos, val, align='center', alpha=5)
+	plt.xticks(y_pos, obj)
+	plt.ylabel('Occurance of Label')
+	plt.title('Labels in Media')
+	plt.show()
+	return 0
+
+def analysis(result):
+	Label_1 = []
+	for x in result:
+		Label_1.append(x[3])
+	occurance = Counter(Label_1)
+	Media_1 = []
+	num_images = 0
+	for y in result:
+		Media_1 = y[2]
+		if Media_1 == 'None':
+			num_images = num_images
+		else:
+			num_images += 1
+
+	return occurance, num_images
+
 def main():
 	filename = "fetched_tweets.json"
 	file = open(filename)
 	os.system("export GOOGLE_APPLICATION_CREDENTIALS='google_credentials.json'")
+
+	test=os.listdir("/home/ece-student/EC_601/Mini_Proj_3/")
+	for item in test:
+	    if item.endswith(".jpg"):
+	        os.remove(item)
 	count = 0
 
 	mydb = mysql.connector.connect(
@@ -75,13 +98,13 @@ def main():
 	try:
 		mycursor.execute("CREATE DATABASE Mini_Proj_3;")
 	except:
-		print("Database exists")
+		print("Database exists appending values")
 
 	try:
 		mycursor.execute("USE Mini_Proj_3")
 		mycursor.execute("CREATE TABLE Twitter (Name VARCHAR(100), Handle VARCHAR(100), Media VARCHAR(200), Label VARCHAR(100));")
 	except:
-		print("Table exists")
+		print("Table exists appending values")
 
 	for lines in file:
 		count = int(count) + 1
@@ -90,25 +113,26 @@ def main():
 		mycursor.execute(sql, val)
 		mydb.commit()
 
-	mycursor.execute("SELECT * FROM Twitter;")
+	mycursor.execute("SELECT * FROM Twitter")
 	result = mycursor.fetchall()
-	print(result)
-	idk = analysis()
-	something = query()
 
+	occurance, num_images = analysis(result)
+	print("\n")
+	obj = []
+	val = []
+	for key in occurance:
+		print(key, " : ", occurance[key])
+		obj.append(key)
+		val.append(occurance[key])
+	y_pos = np.arange(len(obj))
+
+	print("\nTotal Handles: ", len(result))
+	print("Handles with media file: ", num_images)
+
+	print("\nCheck all labels in database")
+	graph(obj, val, y_pos)
+	sys.exit()
 	return 0
-
-def analysis():
-	# mycursor.execute("SELECT Label,COUNT(*) as occurance FROM Twitter GROUP BY Label;")
-	pass
-
-def query():
-	pass
-
-
-
-
-###################################################################
 
 if __name__ == "__main__":
 	main()
